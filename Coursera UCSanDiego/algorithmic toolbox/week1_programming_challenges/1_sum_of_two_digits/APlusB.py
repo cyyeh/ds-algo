8d24ee7839c37952fee853a404760c8bfcb2ff6d
cyyeh <chihyu.jimmy.yeh@gmail.com>
2018-12-14 12:34:04 +0800

    refine week1 programs and add bigO jupyter notebook

--- Coursera UCSanDiego/algorithmic toolbox/week1_programming_challenges/1_sum_of_two_digits/APlusB.py
+++ Coursera UCSanDiego/algorithmic toolbox/week1_programming_challenges/1_sum_of_two_digits/APlusB.py
@@ -5,6 +5,5 @@ def sum_of_two_digits(first_digit, second_digit):
     return first_digit + second_digit
 
 if __name__ == '__main__':
-    input = sys.stdin.readlines()
-    a, b = map(int, input[0].split(' '))
+    a, b = map(int, input().split())
     print(sum_of_two_digits(a, b))
--- Coursera UCSanDiego/algorithmic toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Coursera UCSanDiego/algorithmic toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,12 +1,10 @@
 # python3
-import sys
 
 def max_pairwise_product(numbers):
     numbers = sorted(numbers, reverse=True)
     return numbers[0] * numbers[1]
 
 if __name__ == '__main__':
-    input = sys.stdin.read().splitlines() 
-    input_n = int(input[0])
-    input_numbers = [int(x) for x in input[1].split(' ')]
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
     print(max_pairwise_product(input_numbers))
