1a4ffe818df69c4aaf765abff7bbf8a9c59dd59e
cyyeh <chihyu.jimmy.yeh@gmail.com>
2018-12-23 20:54:00 +0800

    add week 6 slides and update assignments

--- Coursera UCSanDiego/algorithmic toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Coursera UCSanDiego/algorithmic toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,9 +1,19 @@
 # Uses python3
 import sys
 
-def get_change(m):
-    #write your code here
-    return m // 4
+def get_change(money):
+    min_num_coins = [0 for _ in range(money + 1)]
+    coins = [1, 3, 4]
+
+    for m in range(1, money + 1):
+        min_num_coins[m] = 1001
+        for i in range(len(coins)):
+            if m >= coins[i]:
+                num_coins = min_num_coins[m - coins[i]] + 1
+                if num_coins < min_num_coins[m]:
+                    min_num_coins[m] = num_coins
+
+    return min_num_coins[money]
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
--- Coursera UCSanDiego/algorithmic toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ Coursera UCSanDiego/algorithmic toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -2,16 +2,32 @@
 import sys
 
 def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
-    return reversed(sequence)
+    sequence = [0] * (n + 1)
+    sequence[1] = 1
+    sequence_dict = {
+        "1": [1]
+    }
+
+    for i in range(2, n + 1):
+        sequence[i] = n + 1
+        sequence_dict[str(i)] = []
+        if i - 1 > 0:
+            num_sequence = sequence[i - 1] + 1
+            if num_sequence < sequence[i]:
+                sequence[i] = num_sequence
+                sequence_dict[str(i)] = sequence_dict[str(i - 1)] + [i]
+        if i % 3 == 0:
+            num_sequence = sequence[i // 3] + 1
+            if num_sequence < sequence[i]:
+                sequence[i] = num_sequence
+                sequence_dict[str(i)] = sequence_dict[str(i // 3)] + [i]
+        if i % 2 == 0:
+            num_sequence = sequence[i // 2] + 1
+            if num_sequence < sequence[i]:
+                sequence[i] = num_sequence
+                sequence_dict[str(i)] = sequence_dict[str(i // 2)] + [i]
+
+    return sequence_dict[str(n)]
 
 input = sys.stdin.read()
 n = int(input)
