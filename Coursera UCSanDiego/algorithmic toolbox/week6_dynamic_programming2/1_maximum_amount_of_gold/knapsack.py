c132440ec712fcc64eef2e70139effa9222d02d6
cyyeh <chihyu.jimmy.yeh@gmail.com>
2018-12-24 14:20:05 +0800

    update algorithms week6 assignments

--- Coursera UCSanDiego/algorithmic toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ Coursera UCSanDiego/algorithmic toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -2,12 +2,18 @@
 import sys
 
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    T = [[None] * (len(w) + 1) for _ in range(W + 1)]
+
+    for u in range(W + 1):
+        T[u][0] = 0
+
+    for i in range(1, len(w) + 1):
+        for u in range(W + 1):
+            T[u][i] = T[u][i - 1]
+            if u >= w[i - 1]:
+                T[u][i] = max(T[u][i], T[u - w[i - 1]][i - 1] + w[i - 1])
+
+    return T[W][len(w)]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- Coursera UCSanDiego/algorithmic toolbox/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ Coursera UCSanDiego/algorithmic toolbox/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -1,17 +1,50 @@
 # Uses python3
-def evalt(a, b, op):
-    if op == '+':
-        return a + b
-    elif op == '-':
-        return a - b
-    elif op == '*':
-        return a * b
-    else:
-        assert False
+import math
 
 def get_maximum_value(dataset):
-    #write your code here
-    return 0
+    def evalt(a, b, op):
+        if op == '+':
+            return a + b
+        elif op == '-':
+            return a - b
+        elif op == '*':
+            return a * b
+        else:
+            assert False
+
+    def min_and_max(i, j):
+        current_min = 100000
+        current_max = -100000
+        for k in range(i, j):
+            a = evalt(M[i][k], M[k+1][j], dataset[i+1])
+            b = evalt(M[i][k], m[k+1][j], dataset[i+1])
+            c = evalt(m[i][k], M[k+1][j], dataset[i+1])
+            d = evalt(m[i][k], m[k+1][j], dataset[i+1])
+            current_min = min(current_min, a, b, c, d)
+            current_max = max(current_max, a, b, c, d)
+        return (current_min, current_max)
+    
+    M = []
+    m = []
+    for i in range(math.ceil(len(dataset)/2)):
+        temp = []
+        for j in range(math.ceil(len(dataset)/2)):
+            if i == j:
+                temp.append(int(dataset[i * 2]))
+            else:
+                temp.append(None)
+        M.append(temp)
+        m.append(temp)
+
+
+    for s in range(1, len(dataset)):
+        for i in range(len(dataset)-s-1):
+            j = i + s
+            m[i][j], M[i][j] = min_and_max(i, j)
+    
+    print(M)
+
+    return M[0][math.ceil(len(dataset)/2)-1]
 
 
 if __name__ == "__main__":
